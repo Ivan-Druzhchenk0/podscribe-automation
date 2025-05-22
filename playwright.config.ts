3032780652cd2bb80bf40edd01e048113f7bf2eb
Ivan Druzhchenko <ivan.druzhchenko@sysgears.com>
2025-05-22 10:04:22 +0300

    fix: fixed config vars

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  * @see https://playwright.dev/docs/test-configuration
  */
 const config = {
-  expect: { timeout: 10 * 100 },
+  expect: { timeout: 10 * 1000 },
   timeout: 45 * 1000,
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -19,13 +19,14 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : '50%',
+  workers: process.env.CI ? '50%' : '50%',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://app.podscribe.com/',
+    // Set custom attribute to match testId on the page.
     testIdAttribute: 'data-test-id',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
